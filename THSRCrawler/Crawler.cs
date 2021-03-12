608dfff51f129d263bceb730f0bb2606bb0d8a64
Yich <07486.yich.lin@ict888.net>
2021-03-12 14:48:26 +0800

    123

---
+++ THSRCrawler/Crawler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace THSRCrawler
+{
+    public class Crawler
+    {
+        //查詢車次網址 https://irs.thsrc.com.tw/IMINT/?wicket:bookmarkablePage=:tw.com.mitac.webapp.thsr.viewer.History
+        //測試用訂位代號 03352429 
+
+        //登入
+        //變更行程
+        //取得可用車次列表
+    }
+}
