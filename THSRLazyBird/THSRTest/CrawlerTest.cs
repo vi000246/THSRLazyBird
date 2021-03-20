e0625bf32f904b8698247ed413c666d2f2c7219a
yich <>
2021-03-21 00:40:14 +0800

    123

---
+++ THSRLazyBird/THSRTest/CrawlerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using THSRCrawler;
+
+namespace THSRTest
+{
+    [TestClass]
+    public class CrawlerTest
+    {
+        private readonly Crawler _crawler;
+        public CrawlerTest(Crawler crawler)
+        {
+            _crawler = crawler;
+        }
+
+        [TestMethod]
+        public void TestMethod() {
+
+        }
+    }
+}
---
+++ THSRLazyBird/THSRTest/ValidationTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace THSRTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
